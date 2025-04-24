aba36139e6c4d441236d84a66bb0030d2732a53c
daniel <daniel_correa@epam.com>
2025-04-24 00:26:01 -0300

    added cog icon

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Button } from "primereact/button";
 
 export default function NavBar() {
   return (
     <header id="nav-bar">
-      <nav > 
+      <nav className="flex justify-content-between flex-row items-center w-full"> 
         <Image src="/dux-icon.webp" alt="logo" width={45} height={45} className="ml-2 aspect-square"  />
+        <Button text icon="pi pi-cog" className="text-white"/>
       </nav>
     </header>
   );
